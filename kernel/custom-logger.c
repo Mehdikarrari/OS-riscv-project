6b98d55ec92291ca5c47ba62bb4f69aefc14611b
unknown <mehdikarrari04@gmail.com>
2025-04-18 12:21:27 +0330

    debug logger

--- kernel/custom-logger.c
+++ kernel/custom-logger.c
@@ -5,12 +5,12 @@
  
 void log_message(int level, const char *message) { 
     if(level == 3) {
-        printf("INFO-%s",message);
+        printf("INFO-%s\n",message);
     }
     else if(level == 2) {
-        printf("WARNING-%s",message);
+        printf("WARNING-%s\n",message);
     }
     else{
-        printf("ERROR-%s",message);
+        printf("ERROR-%s\n",message);
     } 
 }  
