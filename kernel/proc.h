95ce3ca2ab79359f59389ccaf6208f4d2aa1d9ee
unknown <mehdikarrari04@gmail.com>
2025-06-09 20:37:04 +0330

    change proc data structure

--- kernel/proc.h
+++ kernel/proc.h
@@ -81,6 +81,21 @@ struct trapframe {
 
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+enum threadstate { 
+    THREAD_UNUSED,      
+    THREAD_RUNNABLE, 
+     THREAD_RUNNING,     
+    THREAD_JOINED,      
+    THREAD_SLEEPING     
+}; 
+struct thread { 
+    enum threadstate state;        
+    struct trapframe *trapframe;    
+    uint id;                       
+    uint join;                     
+    int sleep_n;                   
+    uint sleep_tick0;             
+};
 // Per-process state
 struct proc {
   struct spinlock lock;
@@ -104,19 +119,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-};
-enum threadstate { 
-    THREAD_UNUSED,      
-    THREAD_RUNNABLE, 
-     THREAD_RUNNING,     
-    THREAD_JOINED,      
-    THREAD_SLEEPING     
-}; 
-struct thread { 
-    enum threadstate state;        
-    struct trapframe *trapframe;    
-    uint id;                       
-    uint join;                     
-    int sleep_n;                   
-    uint sleep_tick0;             
+   struct thread threads[NTHREAD];     // Array of threads belonging to the process 
+    struct thread *current_thread;      // Pointer to the currently running thread
 };
