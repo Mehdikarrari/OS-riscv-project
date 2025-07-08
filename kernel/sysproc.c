96ee2f13474332ebb55ca30afde0eb1adce008cc
unknown <mehdikarrari04@gmail.com>
2025-07-08 15:35:08 +0330

    add test code

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -53,23 +53,29 @@ sys_sbrk(void)
 uint64
 sys_sleep(void)
 {
-  int n;
-  uint ticks0;
-
-  argint(0, &n);
-  if(n < 0)
-    n = 0;
-  acquire(&tickslock);
-  ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
-      release(&tickslock);
-      return -1;
-    }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
-  return 0;
+  int n; 
+  uint ticks0; 
+ 
+  argint(0, &n); 
+  if(n < 0) 
+    n = 0; 
+  acquire(&tickslock); 
+  ticks0 = ticks; 
+  if (myproc()->current_thread) { 
+      release(&tickslock); 
+      sleepthread(n, ticks0); 
+      return 0; 
+  } 
+ 
+  while(ticks - ticks0 < n){ 
+    if(killed(myproc())){ 
+      release(&tickslock); 
+      return -1; 
+    } 
+    sleep(&ticks, &tickslock); 
+  } 
+  release(&tickslock); 
+  return 0; 
 }
 
 uint64
